3ceda7663fd2eca7ee268ae4e896c9422152328c
thomasschafer <thomasschafer97@gmail.com>
2024-06-04 22:32:25 +0100

    write http header

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,15 @@
+use std::io::prelude::*;
 use std::net::TcpListener;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(_stream) => {
+    for maybe_stream in listener.incoming() {
+        match maybe_stream {
+            Ok(mut stream) => {
+                stream
+                    .write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes())
+                    .unwrap();
                 println!("accepted new connection");
             }
             Err(e) => {
