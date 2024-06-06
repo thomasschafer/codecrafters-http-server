7760b02d88acb6ef5d158fffec29c7e5fa1171ba
thomasschafer <thomasschafer97@gmail.com>
2024-06-06 12:35:46 +0100

    handle /echo request

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
-use anyhow::{anyhow, Result};
+use anyhow::{anyhow, bail, Result};
+use regex::Regex;
 use std::io::prelude::*;
 use std::net::{TcpListener, TcpStream};
 use std::str;
@@ -10,12 +11,29 @@ fn handle_connection(stream: &mut TcpStream) -> Result<()> {
         return Err(anyhow!("Couldn't handle request length"));
     }
 
-    let request_words = str::from_utf8(&buf).unwrap().split(' ').collect::<Vec<_>>();
+    let request_words = str::from_utf8(&buf)
+        .unwrap()
+        .split_whitespace()
+        .collect::<Vec<_>>();
+    let method = request_words[0];
+    if method != "GET" {
+        bail!("Unhandled request type {}", method);
+    }
     let path = request_words[1];
 
-    let response = match path {
-        "/" => "HTTP/1.1 200 OK\r\n\r\n",
-        _ => "HTTP/1.1 404 Not Found\r\n\r\n",
+    let re = Regex::new(r"/echo/(.*)").unwrap();
+    let parsed = re.captures(path);
+    let response = match (parsed, path) {
+        (Some(captures), _) => {
+            let str_to_echo = captures.get(1).map(|m| m.as_str()).unwrap();
+            format!(
+                "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+                str_to_echo.len(),
+                str_to_echo
+            )
+        }
+        (None, "/") => "HTTP/1.1 200 OK\r\n\r\n".to_string(),
+        (None, _) => "HTTP/1.1 404 Not Found\r\n\r\n".to_string(),
     };
     stream.write_all(response.as_bytes())?;
     Ok(())
